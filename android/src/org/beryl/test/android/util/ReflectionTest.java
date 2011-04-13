33c4e1587ac142fdf69a5dd36a5e7ead92ea9312
futonredemption <futonredemption@localhost>
2011-04-13 01:27:46 -0400

    Another unit test.

--- android/src/org/beryl/test/android/util/ReflectionTest.java
+++ android/src/org/beryl/test/android/util/ReflectionTest.java
@@ -1,6 +1,7 @@
 package org.beryl.test.android.util;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -9,9 +10,29 @@ import org.beryl.util.Reflection;
 
 public class ReflectionTest extends TestCase {
 
-	public void test_findMethod_Object_HashCode() {
+	public void test_findMethod_MethodWithZeroParameters() {
 		Method method = tryFindDeclaredMethod(Object.class, "hashCode", null);
 		assertNotNull(method);
+	}
+	
+	public void test_findMethod_MethodWithParameters() {
+		List<Class<?>> params = new ArrayList<Class<?>>();
+		params.add(String.class);
+		Method method = tryFindDeclaredMethod(String.class, "indexOf", params);
+		assertNotNull(method);
+	}
+	
+	public void test_findMethod_MethodWithParameters_InvokeYieldsExpectedResult() {
+		List<Class<?>> params = new ArrayList<Class<?>>();
+		params.add(String.class);
+		Method method = tryFindDeclaredMethod(String.class, "indexOf", params);
+		String testString = "unit-test";
+		try {
+			Integer intVal = (Integer)method.invoke(testString, "test");
+			assertEquals(intVal.intValue(), 5);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
 	}
 	
 	public static Method tryFindDeclaredMethod(final Class<?> clazz, final String methodName, final List<Class<?>> paramTypes) {
